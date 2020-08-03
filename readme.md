d80d127b3e95739fa0604c2765c3bd291f93a62e
Amyavow <amy.avow@gmail.com>
2020-08-03 20:50:31 +0100

    first commit with project setup

---
+++ readme.md
@@ -0,0 +1,49 @@
+# Interview Tutorial API
+
+> An implementation of with Node/Express.
+
+## Features
+
+* User signup
+* User sign in (using JWT)
+* Post interview Question (authenticated user)
+* View interview Questions
+* Edit interview Questions(authenticated user)
+* Delete interview Questions(authenticated user)
+* Upvote or downvote answers (authenticated user)
+* Answer Question (authenticated user)
+* Search Questions
+
+## Stack
+
+* NodeJs with express
+* Authentication with jsonwebtoken
+* MongoDB database
+* Documentation with Swagger
+
+## Installing / Getting started
+
+```shell
+git clone https://github.com/MakeSchool-Tutorials/Interview-Tutorial-API.git
+cd stackoverflow-clone
+npm install
+npm run start-dev # prints Listening on port $portnumber
+```
+
+### Building
+
+```shell
+npm build # build project for prod
+npm start # starts project in prod mode
+```
+
+## Contributing
+
+If you'd like to contribute, please fork the repository and use a feature
+branch. Pull requests are warmly welcome.
+
+The project enforces eslint airbnb code style.
+
+## Routes
+
+TBD
---
+++ src/app.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import debug from 'debug';
+import morgan from 'morgan';
+import { config } from 'dotenv';
+import bodyparser from 'body-parser';
+import mongoose from 'mongoose';
+
+config();
+const app = express();
+
+// setup database connection
+mongoose.connect(process.env.TEST_DB_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false,
+});
+
+// setup morgan
+app.use(morgan('dev'));
+
+// setup body parser
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Welcome to interview Tutorial API, go through documentation to find endpoints for your request'));
+
+// Set up routes for v1
+// app.use('/api/v1/', routes);
+
+// set up route for swagger documentation
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Resource not found.',
+    possibleCauses: [
+      'Maybe you got the URL wrong',
+      '...',
+    ],
+  });
+});
+
+// catch 404 and forward to error handler
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  err.message = 'Sorry, this path doesn\'t exit';
+  next(err);
+});
+
+// error handler
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    err,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  debug('dev')(`Listening on port ${process.env.PORT}`);
+});
+
+module.exports = app;
