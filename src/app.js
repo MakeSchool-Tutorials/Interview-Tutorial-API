192f213a778127cd6667c4ffd432d2c9249ecf03
Amyavow <amy.avow@gmail.com>
2020-08-18 23:27:26 +0100

    fix mongoose uri string bug

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import routes from './v1/routes/index';
 
-config();
+config({ path: '.env' });
 const app = express();
 
 // setup database connection
