de3812191b78e513df8f76940f01206e42eb0e55
Amyavow <amy.avow@gmail.com>
2020-08-18 17:42:19 +0100

    fix regen runtime error

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import routes from './v1/routes/index';
 
-import 'core-js/stable';
+import 'core-js';
 import 'regenerator-runtime/runtime';
 
 config();
