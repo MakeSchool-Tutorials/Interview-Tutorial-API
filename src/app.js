3c19a752ff063615780dc85efd150a0b85a6176c
Amyavow <amy.avow@gmail.com>
2020-08-18 22:23:57 +0100

    add babel-transform-runtime

--- src/app.js
+++ src/app.js
@@ -6,9 +6,6 @@ import bodyparser from 'body-parser';
 import mongoose from 'mongoose';
 import routes from './v1/routes/index';
 
-import 'core-js';
-import 'regenerator-runtime/runtime';
-
 config();
 const app = express();
 
